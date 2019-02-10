69c4354d2ac0f41a4f0f25406fa75ce5f8a907eb
goodronoid <goodronidze@gmail.com>
2019-02-10 23:28:12 +0500

    DataBase files uploaded

---
+++ RequestSystem dbo.Note.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Note] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Title]       NVARCHAR (MAX) NOT NULL,
+    [Description] NVARCHAR (MAX) NOT NULL,
+    [CreateDate]  DATETIME2 (7)  NOT NULL,
+    [Status]      NVARCHAR (MAX) NOT NULL,
+    [History]     NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Note] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
